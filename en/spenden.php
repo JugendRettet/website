abc4f62633405e9d43c8ee8becabd010d85df10c
snscltt <snscltt@cosmofox.net>
2015-12-20 14:06:09 +0100

    spenden: en translation

--- en/spenden.php
+++ en/spenden.php
@@ -2,9 +2,9 @@
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" prefix="og: http://ogp.me/ns#">
     <head>
         <meta name="google-site-verification" content="KMNzTcq9zAtZpPFU4205bFNZx-FZKyiSFaNqrbi4XPI" />
-        <meta name="description" content="Wir sind eine Organisation von jungen Erwachsenen für junge Erwachsene. Wir sammeln Gelder für den Erwerb eines Schiffes und mit diesem Schiff werden wir private Seenotrettung auf dem Mittelmeer betreiben." />
+        <meta name="description" content="We are an organization of young adults for young adults. We collect funds to buy a ship. With this ship, we will conduct private sea rescue on the Mediterranean Sea" />
         <meta name="author" content="Jugend Rettet" />
-        <meta name="keywords" content="Jugend, Rettet, Jugend Rettet, Jugend Rettet e. V., Seenotrettung, Mittelmeer, Flüchtlinge, Spenden, Europa, Organisation" />
+        <meta name="keywords" content="Jugend, Rettet, Jugend Rettet, Jugend Rettet e. V., Seenotrettung, Mittelmeer, Flüchtlinge, Spenden, Europa, Organisation, sea, rescue, Mediterranean, refugee, fund, donate, europe" />
 
         <meta property="og:title" content="Jugend Rettet" />
         <meta property="og:description" content="Wir sind eine Organisation von jungen Erwachsenen für junge Erwachsene. Wir sammeln Gelder für den Erwerb eines Schiffes und mit diesem Schiff werden wir private Seenotrettung auf dem Mittelmeer betreiben." />
@@ -14,7 +14,7 @@
         <meta property="og:site_name" content="Jugend Rettet" />
 
         <meta charset="utf-8">
-        <title>Spenden — Jugend Rettet</title>
+        <title>Donate — Jugend Rettet</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
         <meta http-equiv="language" content="en">
         <link rel="shortcut icon" type="image/x-icon" href="http://jugendrettet.org/graphics/jr.ico"/>
@@ -114,33 +114,33 @@
                 <div id=top>
                 </div>
                 <div class="triple">
-                    <h3>Spenden</h3>
-                    <p>Die Menschen auf dem Mittelmeer sterben jetzt. Deshalb brauchen wir Geld, um unser Schiff zu kaufen und mit der Rettung zu beginnen.</p>
+                    <h3>Donate</h3>
+                    <p>People on the Meditarranean are dying right now. That’s why we need money to buy our ship and to start rescuing.</p>
                 </div>
                 <div class="container triple">
 
                   <div style="margin-right:10px"class="column">
                     <div style="padding-left:5px;padding-right:5px" id="spendenbox">
-                      <h4 id="spendenkonto">Spendenkonto</h4>
-                      <p><b>Empfänger:</b> Jugend Rettet e.V.</p>
-                      <p><b>Kontonummer:</b> 0190465743</p>
+                      <h4 id="spendenkonto">Account for Donations</h4>
+                      <p><b>Beneficiary:</b> Jugend Rettet e.V.</p>
+                      <p><b>Account number:</b> 0190465743</p>
                       <p><b>IBAN:</b> DE28 1005 0000 0190 4657 43</p>
                       <p><b>BIC:</b> BELADEBEXXX</p>
-                      <p><b>Kreditinstitut:</b> Berliner Sparkasse</p>
+                      <p><b>Bank:</b> Berliner Sparkasse</p>
                       <br>
-                      <h4>direkt spenden</h4>
+                      <h4>Donate directly</h4>
                       <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
                       <input type="hidden" name="cmd" value="_s-xclick">
                       <input type="hidden" name="hosted_button_id" value="SBV6JB66SH52G">
                       <input type="image" src="graphics/checkout-logo-69x26-alt-2x.png" name="submit" alt="Jetzt einfach, schnell und sicher online bezahlen – mit PayPal.">
                       </form>
-                      <p style="font-size:small">PayPal erhebt für Spenden 1.5% + 0,35€</p>
+                      <p style="font-size:small">PayPal charges 1.5% + 0,35€ for each donation.</p>
                     </div>
                   </div>
 
                   <div class="column" style="min-width:235px">
-                    <h4>Phasen</h4>
-                    <p>Unser Zeitplan steht. Ab Juni werden wir Menschenleben retten. Die Finanzierung unserer Rettungsmission läuft in drei Phasen ab. Verfolge hier, wie lange es noch ist bis zur ersten Rettungsmission.</p>
+                    <h4>Phases</h4>
+                    <p>Our timetable is set. From June on, we will save human lifes. The funding of our rescue mission will run in three phases. Trace here, how long it will take until our first rescue mission takes off.</p>
                     <br>
                     <div id="phase">
                         <div class="ph-container" id="ph-eins">
@@ -149,78 +149,78 @@
                                 <hr id="ph-eins-progress"></hr>
                             </div>
                             <div class="content" id="ph-eins-content">
-                                <p>Bis zum 15. Februar werden wir die Gelder für den Kauf des Schiffs sammeln. Weitere Infos zu unserem Schiff findest du <a href="schiff#wie">hier</a>.
+                                <p>Until 15th of February we will collect the funds for the purchase of the ship. You can find further information on our ship <a href="schiff#wie">here</a>.
 
-                                Unser Schiff ist 32m lang. Bisher sind <?php echo $value1;?>m finanziert. Spende auch du 10 cm.</p>
+                                Our ship is 32m (105ft) long. By now, <?php echo $value1;?>m are funded. Donate 10cm for yourself!</p>
                                 <br>
-                                <p>Insgesamt fallen für Kauf und Überführung <b>270.000 €</b> an.</p>
+                                <p>Accumulated cost for aquisition and transfer are <b>270.000 €</b>.</p>
                             </div>
                         </div>
                         <div class="ph-container" id="ph-zwei">
                             <div class="header">
-                                <span class="title">Phase 2</span><span class="percentage"><span style="color: red;">Ab dem 16. Feb. 2016</span><!--<?php
+                                <span class="title">Phase 2</span><span class="percentage"><span style="color: red;">From 16th February 2016</span><!--<?php
                                 $phase2 = file_get_contents('donations/phase2.txt');
                                 echo round(100 / 240000 * $phase2);
                                 ?>%--></span>
                                 <hr id="ph-zwei-progress"></hr>
                             </div>
                             <div class="content" id="ph-zwei-content">
-                                <p>Phase 2 läuft bis zum 31. März.</p>
+                                <p>Phase 2 two will run until 31st of March.</p>
                                 <br>
-                                <p>Für die Austattung und den Umbau des Schiffs in Hamburg für unseren Zweck werden wir <b>240.000 €</b> brauchen.</p>
+                                <p>For equipment and conversion of the ship for our needs, we will need <b>240.000 €</b>.</p>
                             </div>
                         </div>
                         <div class="ph-container" id="ph-drei">
                             <div class="header">
-                                <span class="title">Phase 3</span><span class="percentage"><span style="color: red;">Ab dem 1. April 2016</span><!--<?php
+                                <span class="title">Phase 3</span><span class="percentage"><span style="color: red;">From 1st April 2016</span><!--<?php
                                 $phase3 = file_get_contents('donations/phase3.txt');
                                 echo round(100 / 290000 * $phase3);
                                 ?>%--></span>
                                 <hr id="ph-drei-progress"></hr>
                             </div>
                             <div class="content" id="ph-drei-content">
-                                <p>In Phase 3 werden die laufenden Kosten für unsere halbjährige Rettungsmission gesammelt, die im Frühsommer starten soll.</p>
+                                <p>In Phase 3 the running costs for our six-month rescue mission will be collected. The mission is set to take off in early summer.</p>
                                 <br>
-                                <p>Eine halbjährige Rettungsoperation auf dem Mittelmeer wird uns ca. <b>290.000 €</b> kosten.</p>
+                                <p>A six-month rescue mission on the Mediterranean Sea will cost us about <b>290.000 €</b>.</p>
                             </div>
                         </div>
                     </div><br>
                 </div>
                 <div class="column" style="min-width: 300px;">
-                  <h4>Phase 1 - Schiff</h4>
+                  <h4>Phase 1 - Ship</h4>
 
                     <div id="schiff-ani">
                       <div class="schiff-klappe" id="1-klappe">
-                        <b>Schiffsschraube</b>
-                          <p>Im Heck unseres Schiffes finden sich Schiffsschraube und ein Beiboot.</p>
+                        <b>Ship’s propeller</b>
+                          <p>In the rear of the ship are the propeller and a tender.</p>
                       </div>
                       <div class="schiff-klappe" id="2-klappe">
-                        <b>Crewräume/Maschinenraum</b>
-                          <p>Unsere 11 köpfige Crew wird während unserer Rettungsoperation in den Crewräumen unterkommen. Eine kleine Küche, sanitäre Anlagen gehören ebenfalls zu den Crewräumen.</p>
+                        <b>Crew cabin / engine room</b>
+                          <p>During our operation, our ship’s crew of 11 will live in the crew quarters. A small kitchen andsanitary installations also belong to the quarters.</p>
                       </div>
                       <div class="schiff-klappe" id="3-klappe">
-                        <b>Raum Kapitän/Brücke</b>
-                          <p>Die Kabine unseres Kapitäns liegt unmittelbar in der Nähe der Kommandobrücke. Hier werden der Kapitän und sein Ersatz, der Steuermann, in Vier-Stunden-Schichten das Schiff steuern.</p>
+                        <b>Captains quarter / Bridge</b>
+                          <p>The quarter of our captain is in direct reach of the navigating bridge. Here, the captain and his surrogate, the helmsman, will steer the ship in four-hour shifts.</p>
                       </div>
                       <div class="schiff-klappe" id="4-klappe">
-                        <b>Räume medizinische Versorgung</b>
-                          <p>Der Brücke vorgelagert, auf Höhe des Decks liegt ein Raum für die medizinische Versorgung. Auf dem Mittelmeer häufige Symptome wie Unterkühlung, Dehydratation, Kretze oder Schürf-, und Platzwunden werden von unserem Arzt an Bord behandelt.</p>
+                        <b>Medical quarters</b>
+                          <p>In front of the bridge, at deck’s height, is a room for medical supply. Typical symptoms on the Mediterranean Sea, like hypothermia, dehydration, itch or abrasion and lacerasion, will be treated on board by our doctor.</p>
                       </div>
                       <div class="schiff-klappe" id="5-klappe">
-                        <b>Deckfläche mit Hydraulikkran</b>
-                          <p>Der Hydraulikkran hebt unsere beiden Beiboote ins Wasser. Mit denen werden wir die Menschen von ihren Booten zu unserem Mutterschiff holen. Die Fläche an Deck ist für die geretteten Menschen vorgesehen. Frauen, Männer und Familien werden getrennt voneinander untergebracht.</p>
+                        <b>Deck with hydraulic crane</b>
+                          <p>The hydraulic crane lifts our tenders to the water. With those, we will get the people from their boats to our mothership. The space on deck is designated for the rescued people. Women, Men and families will be sheltered separately.</p>
                       </div>
                       <div class="schiff-klappe" id="6-klappe">
-                        <b>Deckfläche</b>
-                          <p>Eine LKW-Plane schützt die Geretteten vor Wind und Wetter. Etwa 90 Personen werden an Deck Platz finden.</p>
+                        <b>Deck</b>
+                          <p>A truck tarpaulin protects the rescued people from wind and weather. On deck will be space for approximately 90 people.</p>
                       </div>
                       <div class="schiff-klappe" id="7-klappe">
-                        <b>Lager für Rettungswesten/Kühllager für Verpflegung</b>
-                          <p>Ein Kühllager für Verpflegung von Crew und Geretteten sowie ein Lager für Schwimmwesten findet sich in diesem Bereich des Schiffes.</p>
+                        <b>Storage for life vests / refrigerated hold for fare</b>
+                          <p>A refrigerated hold for provisions of crew and rescued as well as a storage for life vests is in this part of the ship.</p>
                       </div>
                       <div class="schiff-klappe" id="8-klappe">
-                        <b>Bug/Frischwassertank</b>
-                          <p>In der Spitze des Schiffs finden sich Frischwassertanks, mit denen Crew und Gerettete versorgt werden.</p>
+                        <b>Bow / fresh water tank</b>
+                          <p>In the bow of the ship are fresh water tanks to supply crew and rescued people.</p>
                       </div>
                       <br>
                       <div id=schiff-container style="min-height:133px;width:300px;">
@@ -248,10 +248,10 @@
 
                       </div><br>
                       <br>
-                      <p>Klicke auf die Meterangaben, um zu schauen, wie das Schiff aufgeteilt ist!</p>
+                      <p>Click on the meter marks, to see how the ship is divided.</p>
                     </div>
                     <br>
-                    <h4>Rechner</h4>
+                    <h4>Calculator</h4>
                       <div id="rechner">
                       <input type="Text" id="spendeinput" oninput="rechnen(this.form)"> €
                       <!--
@@ -262,19 +262,21 @@
                       <!-- <span id="aendern-field">!</span><br><br> -->
                       <br><br>
                       <div id="spendentext" style="display:none">
-                        <p id="text-schiffsteil-prozent">Mit deinen <b class="EUR"></b> <b>€</b> kämen <b id="prozent"></b> <b>%</b> zum ersten Schiffsteil - der Schiffsschraube - dazu.</p>
-                        <p id="text-schiffsteil-komplett">Mit deinen <b class="EUR"></b> <b>€</b> wäre das erste Schiffsteil - die Schiffsschraube - komplett!</p>
-                        <p id="text-schiffsteil-mehr">Sogar noch mehr:</p>
+                        <p id="text-schiffsteil-prozent">With your <b class="EUR"></b> <b>€</b> a total of <b id="prozent"></b> <b>%</b> would be added to the first part of the ship – the ship’s propeller.</p>
+                        <p id="text-schiffsteil-komplett">With your <b class="EUR"></b> <b>€</b> the first part of the ship – the ship’s propeller – would be completed!</p>
+                        <p id="text-schiffsteil-mehr">And even more:</p>
 
-                        <p id="text-schiff-meter">Insgesamt wären damit <b id="meter"></b> <b id="unitmeter">m</b> des Schiffs finanziert. Das sind <b id="qmeter"></b> <b id="unitqmeter">m²</b>, die Platz für die Seenotrettung bieten.</p>
+                        <p id="text-schiff-meter">Summed up, with this money <b id="meter"></b> <b id="unitmeter">m</b> of the ship are funded. That's <b id="qmeter"></b> <b id="unitqmeter">m²</b> for the sea rescue.</p>
 
                         <p id="text-schiff-komplett">Mit deinen <b class="EUR"></b> <b>€</b> wäre das gesamte Schiff finanziert! Mit einer Länge von <b>32 m</b> und einer Fläche von <b>160 m²</b> bietet es genug Platz, um mehr als <b>80 Menschen</b> auf einmal aus der Seenot zu retten.</p>
 
-                        <p id="button"><br><br><a id="aendern-btn" onclick="changeColor('#spendenbox');toDiv('#top');">ICH RETTE MIT!</a></p>
+                        <p id="text-schiff-komplett">With your <b class="EUR"></b> <b>€</b> the whole ship would be funded! With a length of <b>32 m / 105ft</b> and <b>160 m² / 1722ft²</b> on deck, there is enough space for the sea rescue of up to <b>80 people</b> at once.</p>
+
+                        <p id="button"><br><br><a id="aendern-btn" onclick="changeColor('#spendenbox');toDiv('#top');">I WANT TO RESCUE TOO!</a></p>
 
                       </div>
                       <div id="spendentext-fehler" style="display:none">
-                        <p>Bitte versuch es nur mit Zahlen.</p>
+                        <p>Please try again with numbers only.</p>
                       </div>
                     </div>
                     <br>
