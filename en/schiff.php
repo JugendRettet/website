df3667554d45305a9aa1dfc76c814e518169c8e1
lolphant <lolphant@users.noreply.github.com>
2016-01-07 22:45:07 +0100

    Update schiff.php

--- en/schiff.php
+++ en/schiff.php
@@ -2,15 +2,15 @@
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" prefix="og: http://ogp.me/ns#">
     <head>
         <title>The Ship — Jugend Rettet</title>
-        <meta name="description" content="Wir sind eine Organisation von jungen Erwachsenen für junge Erwachsene. Wir sammeln Gelder für den Erwerb eines Schiffes und mit diesem Schiff werden wir private Seenotrettung auf dem Mittelmeer betreiben." />
+        <meta name="description" content="We are an organization of young adults. We collect funds to buy a ship. With this ship, we will induce private sea rescue on the Mediterranean Sea." />
         <meta charset="utf-8">
         <meta name="author" content="Jugend Rettet" />
-        <meta name="keywords" content="Jugend, Rettet, Jugend Rettet, Jugend Rettet e. V., Seenotrettung, Mittelmeer, Flüchtlinge, Spenden, Europa, Organisation" />
+        <meta name="keywords" content="Jugend, Rettet, Jugend Rettet, Jugend Rettet e. V., sea rescue, Mediterranean Sea, Refugees, Donations, Europe, Organization" />
         <meta name="google-site-verification" content="KMNzTcq9zAtZpPFU4205bFNZx-FZKyiSFaNqrbi4XPI" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
         
         <meta property="og:title" content="Jugend Rettet" />
-        <meta property="og:description" content="Wir sind eine Organisation von jungen Erwachsenen für junge Erwachsene. Wir sammeln Gelder für den Erwerb eines Schiffes und mit diesem Schiff werden wir private Seenotrettung auf dem Mittelmeer betreiben." />
+        <meta property="og:description" content="We are an organization of young adults. We collect funds to buy a ship. With this ship, we will induce private sea rescue on the Mediterranean Sea." />
         <meta property="og:url" content="http://en.jugendrettet.org/schiff" />
         <meta property="og:image" content="http://jugendrettet.org/images/jr-banner.jpg" />
         <meta property="og:type" content="website" />
@@ -137,8 +137,8 @@
                 </div>
                 <div class="container single">
                     <div class="column cushycms">
-                        <p>We are an organisation of young adults, who raise money to buy a ship. With this ship we will conduct rescue missions on the Mediterranean. We will then localize the people, take them in safely at the closest port. Our goal is clear: Less deaths in the Mediterranean.</p>
-                        <p>Furthermore we intend to create a discussion platform via the <a href="europe">Jugend Rettet europe</a> program, in which the asylum policies are debated and scrutinized. We believe in the potential capacity and capabilities of our young generation to change the status quo of the Mediterranean for the better and to become more engaged in the discussions on Asylum policy.</p><br>
+                        <p>We are an organisation of young adults, who raise money to buy a ship. With this ship we will conduct rescue missions on the Mediterranean. We will then localize the people, take them in safely at the closest port. Our goal is clear: Less deaths in the Mediterranean Sea.</p>
+                        <p>Furthermore we intend to create a discussion platform via the <a href="europe">Jugend Rettet europe</a> program, in which the asylum policies are debated and scrutinized. We believe in the potential capacity and capabilities of our young generation to change the status quo of the Mediterranean for the better and to become more engaged in the discussions on asylum policy.</p><br>
                     </div>    
                 </div>
             </div>
@@ -158,7 +158,7 @@
                             
                             -->
                             <img class="portrait" src="http://jugendrettet.org/images/Lena.jpg" alt="Lena">
-                            <p>Lena: "I have been for long involved in an asylum hostel in which many people live, who have fled via the Mediterranean. It was then that I got the wish to commit myself to helping people on the Mediterranean. At <i>Jugend Rettet</i> I found the concrete action field, to constructively engage in for my political ideals."
+                            <p>Lena: "I have been for long involved in an asylum hostel in which many people live, who have fled via the Mediterranean. It was then that I got the wish to commit myself to helping people on the Mediterranean. At <i>Jugend Rettet</i> I found the concrete action field, to constructively engage for my political ideals."
                             </p><br>
                             <!--
                             
@@ -166,17 +166,17 @@
                             
                             -->
                             <img class="portrait" src="http://jugendrettet.org/images/Jakob.jpg" alt="Jakob">
-                            <p>Jakob: "No longer can I pass by a news of a ship mishap quietly. <i>Jugend Rettet</i> is a coalition of young people with the potential to change something. Europe-wide we link and connect motivated younf people and show them how important it is to come up with a plan for a humane asylum policy. A ship is no long term solution however it will save lives and would give rise to the question, why is it that instead of the European governments, it is the youth, who on their private initiative, have to take on this mission."</p><br>
-                            <p>Mohammad, 24 from Homs, Syria: „The revolution started in my city on the 15th March 2011. Firstly the university was closed, there I studied construction. By the 10th of March 2012 I fled to Egypt. I had to avoid being conscripted by the Syrian governments army. I knew that I would have to kill people there and that is something I am not willing to do. Yet life in Egypt was not easy. I had to work for small wages and everyday was a fight for survival. Andi t was not going to get better in the future. The life in Egypt is not easy fort he Syrians, yet also the Egyptians. Therefore the boat to Europe was my only way out."</p>
+                            <p>Jakob: "No longer can I pass by a news of a ship mishap quietly. <i>Jugend Rettet</i> is a coalition of young people with the potential to change something. Europe-wide we link and connect motivated young people and show them how important it is to come up with a plan for a humane asylum policy. A ship is no long term solution. However it will save lives and would give rise to the question, why is it that instead of the European governments, it is the youth, who on their private initiative, have to take on this mission."</p><br>
+                            <p>Mohammad, 24 from Homs, Syria: „The revolution started in my city on the 15th March 2011. First the university was closed, there I studied construction. By the 10th of March 2012 I fled to Egypt. I had to avoid being conscripted by the Syrian governments army. I knew that I would have to kill people there and that is something I am not willing to do. Yet life in Egypt was not easy. I had to work for small wages and every day was a fight for survival. And it was not going to get better in the future. The life in Egypt is not easy for the Syrians, yet also the Egyptians. Therefore the boat to Europe was my only way out."</p>
                         </div>
                         <a class="mehr-btn">more ...</a>
                     </div>
                     <div class="column cushycms mehr" id="sit">
                         <h4><img class="icon" src="http://jugendrettet.org/graphics/sit.svg" alt="Aktuelle Situation">Current Situation</h4>  
-                        <p>In 2014 218.000 People have fled via the Mediterranean and sought refuge. 3500 of them didn’t make it.</p>
+                        <p>In 2014, 218.000 People have fled via the Mediterranean and sought refuge. 3500 people did not survive their journey.</p>
                         <div class="mehr-klappe"><br>
                             <p>The rate of failure is high. In 2015 there should be between 500,000 to 1 million people daring to escape via the Mediterranean, according to Frontex. The central Mediterranean remain the most important escape route. It starts from Libya and ends in Lampedusa, Malta or Sardinia and has been used by 78% of the refugees in 2014. This huge number is the result of the closed European external borders and embassies that permit only the illegal and hence dangerous entrance into Europe as the last option. (<a href="http://www.zeit.de/gesellschaft/zeitgeschehen/2015-03/frontex-rekordzahl-fluechtlinge" target="_blank">ZEIT, März 2015</a>)</p><br>
-                            <p>60% of the Young people declared themselves ready to work with like-minded people on the issue, if they feel they are able to make a change (<a href="http://www.dgb.de/themen/++co++7525a922-220a-11df-7d31-00093d10fae2" target="_blank">DGB, September 2009</a>). Every 4th young person is eager to participate in concrete projects in the city or community of his/her. As of now there has not been a comparable Youth Organization with the objective of conducting private rescue missions in the Mediterranean. Concrete independent initiatives for young people, through which one can express and exercise political will and determination, (<a href="http://www.spiegel.de/schulspiegel/leben/kinderhilfswerk-umfrage-kinder-wuerden-sich-gern-staerker-engagieren-a-890245.html">SPIEGEL Online, März 2013</a>). Aktuell gibt es keine vergleichbare von Jugendlichen initiierte Organisation mit dem Ziel der privaten Seenotrettung auf dem Mittelmeer. Konkrete, unabhängige Aktionsfelder für junge Erwachsene fehlen häufig, wodurch der Wille, sich politisch zu engagieren, oft nicht umgesetzt werden kann.</p>
+                            <p>60% of the Young people declared themselves ready to work with like-minded people on the issue, if they feel they are able to make a change (<a href="http://www.dgb.de/themen/++co++7525a922-220a-11df-7d31-00093d10fae2" target="_blank">DGB, September 2009</a>). Every 4th young person is eager to participate in concrete projects in the city or community of his/her. As of now, there has not been a comparable Youth Organization with the objective of conducting private rescue missions in the Mediterranean. Concrete independent initiatives for young people, through which one can express and exercise political will and determination, (<a href="http://www.spiegel.de/schulspiegel/leben/kinderhilfswerk-umfrage-kinder-wuerden-sich-gern-staerker-engagieren-a-890245.html">SPIEGEL Online, März 2013</a>). Aktuell gibt es keine vergleichbare von Jugendlichen initiierte Organisation mit dem Ziel der privaten Seenotrettung auf dem Mittelmeer. Konkrete, unabhängige Aktionsfelder für junge Erwachsene fehlen häufig, wodurch der Wille, sich politisch zu engagieren, oft nicht umgesetzt werden kann.</p>
                         </div>
                         <a class="mehr-btn">more ...</a>
                     </div>
