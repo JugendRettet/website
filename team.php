290fc6993ee37bd7b806f30d0eed115f56f53ff6
snscltt <snscltt@cosmofox.net>
2016-02-22 10:06:58 +0100

    team: fix some errors

--- team.php
+++ team.php
@@ -92,11 +92,11 @@
                         <ul>
                             <li><b>Jakob</b>: Gründer und 1. Vorsitzender
                                 <br>
-                                Tel.: <a href="tel:+49 160 95 68 38 73">+4916095683873</a>
+                                Tel.: <a href="tel:+4916095683873">+49 160 95 68 38 73</a>
                             </li>
                             <li><b>Lena</b>: Gründerin und 2. Vorsitzender
                                 <br>
-                                Tel.: <a href="tel:+49 151 68 44 63 98">+4915168446398</a>
+                                Tel.: <a href="tel:+4915168446398">+49 151 68 44 63 98</a>
                             </li>
                         </ul>
                         <br>
