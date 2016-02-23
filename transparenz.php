d9759619ffaa21284b7ebc212ca2f6fec90a88e7
snscltt <snscltt@cosmofox.net>
2016-02-23 16:37:16 +0100

    transparenz: add dummy link back to en

--- transparenz.php
+++ transparenz.php
@@ -180,6 +180,7 @@
                             <li><a href="impressum">Impressum</a></li>
                             <li style="box-shadow: 0 1px #fff;"><a href="transparenz">Transparenz</a></li>
                             <li class="br"></li>
+                            <li><a href="http://en.jugendrettet.org/spenden">en</a></li>
                         </ul>
                     </nav>
                 </footer>
