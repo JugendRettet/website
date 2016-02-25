5dcd9c5ef8f4a3b179cc667196c64e21205ef697
snscltt <snscltt@cosmofox.net>
2016-02-25 13:15:04 +0100

    update ambassadors

--- news.php
+++ news.php
@@ -428,7 +428,7 @@
                         <ul>
                           <li>&#10003; Wir haben Schiffe unseres gewünschten Schiffstyps - holländische Fischtrawler - besichtigt. Sobald wir uns für ein Schiff entschieden haben erstellen wir die Pläne für den Umbau im April.</li>
                           <!--<li>&#10003; Wir haben eine 11-köpfige Crew inklusive Kapitän gefunden und gemeinsam unsere halbjährige Operation geplant.</li>-->
-                          <li>&#10003; Wir haben ein Netz von Botschaftern in 25 Städten aufgebaut.</li>
+                          <li>&#10003; Wir haben ein Netz von Botschaftern in 27 Städten aufgebaut.</li>
                           <li>&#10003; Wir haben Maria Furtwängler, Armin Rohde und Jasmin Gerat als Stimmen für <i>Jugend Rettet</i> gewinnen können.</li>
                           <li>&#10003; Wir haben in Berlin ein Kernteam von 11 Menschen aufgebaut.</li>
                         </ul>
