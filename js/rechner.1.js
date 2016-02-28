780d3b43bf8cd87a5f654e4cecca8a8fd0ca6d33
snscltt <snscltt@cosmofox.net>
2016-02-28 22:41:53 +0100

    update funds

--- js/rechner.1.js
+++ js/rechner.1.js
@@ -6,7 +6,7 @@ function rechnen(){
 	var bedarf = 62250
 
 	var kosten = 80000
-	var kampagnengeld = 2209
+	var kampagnengeld = 2419
 	var betterplacegeld = +kampagnengeld+0
 	var startgeld = 32697
 	var stand = +betterplacegeld+startgeld
