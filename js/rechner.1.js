e0ce685fd04ffb922edc62729b6ecd0e98c68c14
snscltt <snscltt@cosmofox.net>
2016-03-07 08:55:09 +0100

    update funds

--- js/rechner.1.js
+++ js/rechner.1.js
@@ -6,11 +6,11 @@ function rechnen(){
 	//var bedarf = 62250
 
 	var kosten = 80000
-	var kampagnengeld = 2728.02
+	var kampagnengeld = 2788.02
 	//var betterplacegeld = +kampagnengeld+3349.98
 	//var spakageld = 35122
 	//var stand = +betterplacegeld+spakageld
-	var stand = 41220
+	var stand = 41280
 
 	var balkenwidth = 300
 	var geldbalkenwidth = 260
