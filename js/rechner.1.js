9b32d0f119379834034ce7d0529a8b0d15980611
snscltt <snscltt@cosmofox.net>
2016-03-07 14:52:27 +0100

    update funds

--- js/rechner.1.js
+++ js/rechner.1.js
@@ -6,11 +6,11 @@ function rechnen(){
 	//var bedarf = 62250
 
 	var kosten = 80000
-	var kampagnengeld = 73.02
+	var kampagnengeld = 333.02
 	//var betterplacegeld = +kampagnengeld+3349.98
 	//var spakageld = 35122
 	//var stand = +betterplacegeld+spakageld
-	var stand = 41365
+	var stand = 41625
 
 	var balkenwidth = 300
 	var geldbalkenwidth = 260
--- spenden.php
+++ spenden.php
@@ -186,7 +186,7 @@
                     </div>
 
                     <div class="column hideme" style="min-width: 300px;">
-                        <h4>SPENDENSTAND: 41.365,61€</h4>
+                        <h4>SPENDENSTAND: 41.625,61€</h4>
                         <div id="schiff-ani">
                             <div class="schiff-klappe" id="1-klappe">
                                 <b>Schiffsschraube</b>
@@ -225,7 +225,7 @@
                                 <div id="schiff" style="position:absolute">
                                   <img src="./graphics/schiff-starkekonturen-weiss.svg" alt="Das Schiff" style="width:300px;opacity:0.4">
                                 </div>
-                                <div class="blink balken" id="schiff-gespendet" style="position:absolute;overflow:hidden;width:calc(300px * 41365 / 80000);">
+                                <div class="blink balken" id="schiff-gespendet" style="position:absolute;overflow:hidden;width:calc(300px * 41625 / 80000);">
                                   <img src="./graphics/schiff-blau-starkekontur.svg" alt="Das Schiff" style="width:300px" class="blink">
                                 </div>
                                 <div style="position:absolute;width:300px;">
@@ -241,7 +241,7 @@
                                 </div>
                                 <div style="position:absolute;width:300px">
                                     <!--<span id="0" class="unterteilungen">0m</span>-->
-                                    <span id="stand" class="spendenstand schiff" style="white-space: nowrap;position:absolute;left: calc(-15px + 300px*41365/80000)">41.4 Tsd. €</span>
+                                    <span id="stand" class="spendenstand schiff" style="white-space: nowrap;position:absolute;left: calc(-15px + 300px*41625/80000)">41.6 Tsd. €</span>
                                 </div>
                             </div>
                         </div>
