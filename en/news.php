6ef24e20e6150d1bd2e52113f78762ed0515897c
alexh-name <1@alexh.name>
2016-07-13 15:28:17 +0200

    update embassies

--- en/news.php
+++ en/news.php
@@ -509,7 +509,7 @@
                           <li>&#10003; We inspected ships of our chosen kind, the dutch fishtrawler and decided on one.</li>
                           <!--<li>&#10003; We found an 11-person crew including captain and planned our six month mission.</li>-->
                           <li>&#10003; We bought the ship in May, reconstructed her and named her IUVENTA</li>
-                          <li>&#10003; We built a network of 42 ambassadors in 37 European cities.</li>
+                          <li>&#10003; We built a network of 38 ambassadors in 30 European cities.</li>
                           <li>&#10003; We won German actors like Jan Josef Liefers, Maria Furtwängler, Armin Rohde and Jasmin Gerat as advocates for <i>Jugend Rettet</i>.</li>
                           <li>&#10003; We established a core team of 11 people in Berlin.</li>
                           <li>&#10003; We succesfully finished funding phases 1 and 2 and the reconstruction to send the IUVENTA to her first rescue mission.</li>
