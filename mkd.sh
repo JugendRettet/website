c0b23f2db042e729002b5e25509fbc27bf0531dd
alexh-name <1@alexh.name>
2016-11-24 12:57:26 +0100

    translate: organize into folders

--- mkd.sh
+++ mkd.sh
@@ -3,6 +3,6 @@
 in=$1
 
 sed -e 's/^[\t ]*</<!--mkd--></g' -e 's/>[\t ]*$/><!--mkd-->/g' "${in}" \
-| ./translate.sh ./translations/de_DE.md \
+| ./translate.sh ./translations/de_DE \
 | ./Markdown.pl \
 | sed -e 's/<!--mkd-->//g'
