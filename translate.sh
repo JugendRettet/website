b66c5f17942382e2577a56777aa79ed2614d1be6
alexh-name <1@alexh.name>
2017-02-17 22:46:23 +0100

    iron out translate.sh
    * source all translation files
    * correctly handle '&'
    * better syntax

--- translate.sh
+++ translate.sh
@@ -8,23 +8,26 @@ TEXT="${in}"
 
 translations_dir=$1
 
-. "${translations_dir}"/*
+files="$( find "${translations_dir}"/ -type f )"
+while read file; do
+  . "${file}"
+done <<<"${files}"
 
 declare NEW_TEXT
 
 # All phrases that shall be replaced.
-snippets=$( grep -o -E "_\(.+\)" <<<"${in}" )
+snippets="$( grep -o -E "_\(.+\)" <<<"${in}" )"
 
 while read snippet; do
   # Get the name of the translated phrase in the translations file.
-  translation_name=$( awk '{gsub(/^_\(|\)$/,"");print}' <<<"${snippet}" )
+  translation_name="$( awk '{gsub(/^_\(|\)$/,"");print}' <<<"${snippet}" )"
   # Put translation into variable.
-  translation_content="${!translation_name}"
+  translation_content="$( sed 's/\&/\\\\&/g' <<<"${!translation_name}" )"
   # Replace current phrase with translation.
-  NEW_TEXT=$(
+  NEW_TEXT="$(
     gawk -v n="_\\\("${translation_name}"\\\)" -v c="${translation_content}" \
     '{gsub(n,c)}1' <<<"${TEXT}"
-  )
+  )"
   TEXT="${NEW_TEXT}"
 done <<<"${snippets}"
 
