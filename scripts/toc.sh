c36ab35aa2dab21afd0ebebfd40d3b597ee7923d
alexh-name <git@alexh.name>
2017-03-21 20:21:23 +0100

    name attr in headers not allowed yet

--- scripts/toc.sh
+++ scripts/toc.sh
@@ -5,7 +5,7 @@ FILE="${WSH_GENF_FINAL}"
 TOC="$(
 printf "%s\n" '<ul>'
   grep -F '<h2' "${FILE}" | \
-  LC_ALL=C sed -e 's/h2/a/g' -e 's/name="/href="#/g' -e 's/^/<li>/g' \
+  LC_ALL=C sed -e 's/h2/a/g' -e 's/id="/href="#/g' -e 's/^/<li>/g' \
     -e 's/$/<\/li>/g'
 printf "%s\n" '</ul>'
 )"
