dadc2d391416f01a52fb842c18cacea4cf838e48
alexh-name <git@alexh.name>
2017-03-21 20:38:13 +0100

    dont overscroll even h3

--- extras/js/smoothscroll.js
+++ extras/js/smoothscroll.js
@@ -5,7 +5,7 @@ $(function() {
       target = target.length ? target : $('[id=' + this.hash.slice(1) +']');
       if (target.length) {
         $('html, body').animate({
-          scrollTop: target.offset().top - 45 // Little more than the nav's height
+          scrollTop: target.offset().top - 75 // Little more than the nav's height
         }, 1000);
         return false;
       }
