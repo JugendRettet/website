a48505b642a0aabf8f08e00f89a772e004188411
alexh-name <git@alexh.name>
2017-03-21 15:11:14 +0100

    Revert "prepare launch"
    This reverts commit e9f89b7e13b8a240fa76dc95e2fc36d1f5439630.

--- sync.sh
+++ sync.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 echo 'Syncing to server'
-rsync --exclude-from='.gitignore' -aPq --del dest/ ship@web1.jugendrettet.org:~/vwww/jugendrettet.org/
+rsync --exclude-from='.gitignore' -aPq --del dest/ ship@web1.jugendrettet.org:~/vwww/new.jugendrettet.org/
 echo 'Done syncing.'
